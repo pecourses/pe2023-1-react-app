11945c287f08c8d5d54173b43fa531eec83507ca
Viktoriia <pecourses>
2023-10-14 19:17:18 +0300

    Add lists and keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 
-// реалізувати header, який містить логотип і
-// або фото користувача, якщо він зологінився,
-// або кнопки login/registration
 function App() {
   const isLogin = true;
 
-  return <Header isLogin={isLogin} />;
+  const numbers0 = [1, 2, 3];
+  // const numbers = [<li>1</li>, <li>2</li>, <li>3</li>];
+
+  const numbers = numbers0.map((n, i) => <li key={i}>{n}</li>);
+
+  // return <Header isLogin={isLogin} />;
+  return <ul>{numbers}</ul>;
 }
 
 export default App;
