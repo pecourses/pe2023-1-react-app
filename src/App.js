8dd3d2c1b45179969f9991203be195080fd4c6c7
Viktoriia <pecourses>
2023-10-14 19:44:03 +0300

    Add navList example

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Header from './components/Header';
 function App() {
   const isLogin = true;
 
-  const numbers0 = [1, 2, 3];
-  // const numbers = [<li>1</li>, <li>2</li>, <li>3</li>];
+  const navList = [
+    { aTitle: 'Home', src: '#' },
+    { aTitle: 'About', src: '#' },
+  ];
 
-  const numbers = numbers0.map((n, i) => <li key={i}>{n}</li>);
-
-  // return <Header isLogin={isLogin} />;
-  return <ul>{numbers}</ul>;
+  return <Header isLogin={isLogin} navList={navList} />;
 }
 
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,11 +3,22 @@ import logo from './logo.jpeg'
 import styles from './Header.module.css'
 
 function Header (props) {
-  const { isLogin } = props
+  const { isLogin, navList } = props
+
+  const mapLinks = (link, i) => (
+    <li key={i}>
+      <a href={link.src}>{link.aTitle}</a>
+    </li>
+  )
 
   return (
     <header className={styles.container}>
       <img className={styles.logo} src={logo} alt='logo' />
+
+      <nav>
+        <ul className={styles.linksList}>{navList.map(mapLinks)}</ul>
+      </nav>
+
       {isLogin ? (
         <img className={styles.userImg} src={userIcon} alt='user' />
       ) : (
