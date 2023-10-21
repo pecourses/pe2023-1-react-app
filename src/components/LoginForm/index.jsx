d95267e4e6a5380637fdef5b504a9e7324a46cf4
Viktoriia <pecourses>
2023-10-21 17:23:04 +0300

    Add login form

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,26 +1,71 @@
 import { useState } from 'react'
+import classNames from 'classnames'
+import styles from './LoginForm.module.css'
+
+const LOGIN_FORM_REG_EXP = {
+  login: /^.+@.+$/,
+  password: /^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!#%._].*).{8,16}$/
+}
 
 function LoginForm () {
-  const [test, setTest] = useState('sdsdasd')
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
 
-  console.log('test :>> ', test)
+  function handleLoginChange ({ target: { value } }) {
+    setLogin(value)
+  }
 
+  function handlePasswordChange ({ target: { value } }) {
+    setPassword(value)
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault() // відміна дефолтної поведінки
+    // send request
+    setLogin('')
+    setPassword('')
+  }
+
+  const loginClassName = classNames(styles.formInput, {
+    [styles.valid]: LOGIN_FORM_REG_EXP.login.test(login),
+    [styles.invalid]: !LOGIN_FORM_REG_EXP.login.test(login)
+  })
+
+  const passwordClassName = classNames(styles.formInput, {
+    [styles.valid]: LOGIN_FORM_REG_EXP.password.test(password),
+    [styles.invalid]: !LOGIN_FORM_REG_EXP.password.test(password)
+  })
   return (
-    <div>
-      <input
-        name='test'
-        value={test}
-        onChange={e => {
-          setTest(e.target.value)
-        }}
-      />
-      <button
-        onClick={() => {
-          setTest('')
-        }}
-      >
-        Reset
-      </button>
+    <div className={styles.formContainer}>
+      <h2 className={styles.formTitle}>Login Form</h2>
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <label className={styles.formLabel}>
+          <span className={styles.labelCaption}>Login:</span>
+          <input
+            className={loginClassName}
+            type='email'
+            name='login'
+            value={login}
+            onChange={handleLoginChange}
+            placeholder='your@mail'
+            autoFocus
+          />
+        </label>
+        <label className={styles.formLabel}>
+          <span className={styles.labelCaption}>Password:</span>
+          <input
+            className={passwordClassName}
+            type='password'
+            placeholder='your password'
+            name='password'
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <button className={styles.loginBtn} type='submit'>
+          Login
+        </button>
+      </form>
     </div>
   )
 }
