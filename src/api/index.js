aa0dafd207dbdfb76276f66c90b9287da410686c
Viktoriia <pecourses>
2023-10-22 17:27:43 +0300

    Refactor load users with query-string

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+import queryString from 'query-string';
 import CONFIGS from './../configs';
 
 export function loadUsers(options) {
@@ -5,15 +6,15 @@ export function loadUsers(options) {
     page: 1,
     results: 10,
     baseUrl: CONFIGS.BASE_URL,
-    resultsOrder: CONFIGS.RESULTS_ORDER,
+    seed: CONFIGS.RESULTS_ORDER,
   };
 
-  const realOptions = {
+  const { baseUrl, ...realOptions } = {
     ...defaultOptions,
     ...options,
   };
 
-  return fetch(
-    `${realOptions.baseUrl}?results=${realOptions.results}&page=${realOptions.page}&seed=${realOptions.defaultOptions}`
-  ).then(response => response.json());
+  return fetch(`${baseUrl}?${queryString.stringify(realOptions)}`).then(
+    response => response.json()
+  );
 }
