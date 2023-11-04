16801053078f9c0b44da79871f13572bcb537357
Viktoriia <pecourses>
2023-11-04 18:09:55 +0200

    Add Form, Field, ErrorMessage

--- src/components/UserForm/index.jsx
+++ src/components/UserForm/index.jsx
@@ -1,23 +1,10 @@
-import { Formik } from 'formik'
-import * as yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { USER_VALIDATION_SCHEMA } from '../../utils/validationSchemas'
 
-const USER_VALIDATION_SCHEMA = yup.object({
-  userName: yup
-    .string()
-    .min(2)
-    .max(64)
-    .matches(/^[A-Z][a-z]*$/)
-    .required()
-})
-
-// Formik взяв на себе керуванням станом керованого компонента
-// + містить шаблонний код його обробників
 function UserForm () {
   const initialValues = { userName: '' }
-  // const [name, setName] = useState('')
 
   const handleSubmit = (values, formikBag) => {
-    console.log('values :>> ', values)
     formikBag.resetForm()
   }
 
@@ -27,29 +14,15 @@ function UserForm () {
       onSubmit={handleSubmit}
       validationSchema={USER_VALIDATION_SCHEMA}
     >
-      {formikProps => {
-        console.log(formikProps)
-        return (
-          <form
-            onSubmit={formikProps.handleSubmit}
-            onReset={formikProps.handleReset}
-          >
-            <input
-              type='text'
-              name='userName'
-              value={formikProps.values.userName}
-              onChange={formikProps.handleChange}
-            />
-            {formikProps.errors.userName && (
-              <span>{formikProps.errors.userName}</span>
-            )}
-            <button type='submit'>Send</button>
-            <button type='reset' disabled={!formikProps.dirty}>
-              Reset
-            </button>
-          </form>
-        )
-      }}
+      <Form>
+        <label>
+          <span>Name:</span>
+          <Field type='text' name='userName' />
+        </label>
+        <ErrorMessage name='userName' />
+        <button type='submit'>Send</button>
+        <button type='reset'>Reset</button>
+      </Form>
     </Formik>
   )
 }
